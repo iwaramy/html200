28f14f56411ee698b41e1fd3391df6f6ce88d1c1
Yogi <yugeshiwaram@YUGESHs-iMac.local>
2019-02-11 19:34:45 -0800

    code add

---
+++ styles.js
@@ -0,0 +1,93 @@
+function murphyAlert() {
+
+  let murphyAlert = alert ('Meet Murphy, a 4-year-old. Gender:Female  $123.45')
+
+  console.log(murphyAlert);
+
+}
+
+function poppyAlert() {
+
+  let poppyAlert = alert ('Meet Poppy, a 5-year-old. Gender:Female $123.45')
+
+  console.log(poppyAlert);
+
+}
+
+function jackAlert() {
+
+  let jackAlert = alert ('Meet Jack, a 8-year-old. Gender:Female  $123.45')
+
+  console.log(jackAlert);
+
+}
+function duffyAlert() {
+
+  let duffyAlert = alert ('Meet Duffy, a 3-year-old. Gender:Female  $123.45')
+
+  console.log(duffyAlert);
+
+}
+function lucasAlert() {
+
+  let lucasAlert = alert ('Meet Lucas, a 4-year-old. Gender:Female $123.45')
+
+  console.log(lucasAlert);
+
+}
+function jakeAlert() {
+
+  let jakeAlert = alert ('Meet Jake, a 5-year-old. Gender:Female  $123.45')
+
+  console.log(jakeAlert);
+
+}
+function angusAlert() {
+
+  let angusAlert = alert ('Meet Angus, a 7-year-old. Gender:Female  $123.45')
+
+  console.log(angusAlert);
+
+}
+function violetAlert() {
+
+  let violetAlert = alert ('Meet Vilet, a 6-year-old. Gender:Female  $123.45')
+
+  console.log(violetAlert);
+
+}
+function piperAlert() {
+
+  let piperAlert = alert ('Meet Piper, a 3-year-old. Gender:Female  $123.45')
+
+  console.log(piperAlert);
+
+}
+function maximusAlert() {
+
+  let maximusAlert = alert ('Meet Maximus, a 4-year-old. Gender:Female  $123.45')
+
+  console.log(maximusAlert);
+
+}
+function lunaAlert() {
+
+  let lunaAlert = alert ('Meet Luna, a 2-year-old. Gender:Female  $123.45')
+
+  console.log(lunaAlert);
+
+}
+function stellaAlert() {
+
+  let stellaAlert = alert ('Meet Stella, a 4-year-old. Gender:Female  $123.45')
+
+  console.log(stellaAlert);
+
+}
+
+let total = 0;
+
+function runningTotal() {
+  total = total + 123.45;
+  alert('Your total is $' + total)
+}
