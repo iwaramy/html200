d497754d7ffd9e81d3ff6425d9b8946ae74bb01e
Yogi <yugeshiwaram@YUGESHs-iMac.local>
2019-03-19 01:19:04 -0700

    code added

---
+++ adopt-a-dog/blogs.js
@@ -0,0 +1,31 @@
+const blogPosts = [
+  {
+    imgSrc: 'images/patrick-hendry-221863-unsplash.jpg',
+    heading: 'Traveling With Your Dog',
+    paragraph1:'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.',
+    paragraph2: 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.'
+  },
+  {
+    imgSrc: 'images/matt-nelson-259365-unsplash.jpg',
+    heading: 'How To Walk Multiple Dogs',
+    paragraph1:'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.',
+    paragraph2: 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.'
+  },
+  {
+    imgSrc: 'images/wyatt-ryan-367017-unsplash.jpg',
+    heading: 'Teach Your Dog To Fetch!',
+    paragraph1:'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.',
+    paragraph2: 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.'
+  }
+];
+
+for (let i = 0; i < blogPosts.length; i++) {
+  let blogImg = document.getElementById('blog-img'+ i);
+  blogImg.src = blogPosts[i].imgSrc;
+  console.log(blogImg);
+
+  let blogPost = document.getElementById('blog-content'+ i);
+  console.log(blogPost);
+  blogPost.innerHTML = '<h3>' + blogPosts[i].heading + '</h3><p>' + blogPosts[i].paragraph1
+                       + '</p><p>' + blogPosts[i].paragraph2 + "</p>";
+}
